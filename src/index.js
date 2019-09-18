df29adb159fa22526f960c19422d906f3470d567
Jackson Callaway <jackson.callaway@coinbase.com>
2019-09-17 20:44:46 -0700

    bootstrapping

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => {
+  return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<Root />, document.querySelector("#react-root"));
